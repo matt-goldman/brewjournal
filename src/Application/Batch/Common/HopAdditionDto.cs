8cc5deba9469f5a3019627bdeb1e598d5bcdc6f9
Matt Goldman <MattGoldman@ssw.com.au>
2020-06-30 21:33:51 +1000

    Added hop and sample dto properties

--- src/Application/Batch/Common/HopAdditionDto.cs
+++ src/Application/Batch/Common/HopAdditionDto.cs
@@ -1,12 +1,22 @@
-﻿using brewjournal.Application.Common.Mappings;
+﻿using AutoMapper;
+using brewjournal.Application.Common.Mappings;
 using brewjournal.Domain.Entities;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace brewjournal.Application.Batch.Common
 {
     public class HopAdditionDto : IMapFrom<HopAddition>
     {
+        public string IngredientName { get; set; }
+        public int IngredientId { get; set; }
+        public int? Minutes { get; set; }
+        public DateTime? DateAdded { get; set; }
+        public int? Temperature { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<HopAddition, HopAdditionDto>()
+                .ForMember(d => d.IngredientName, opt => opt.MapFrom(src => src.Ingredient.Name));
+        }
     }
 }
--- src/Application/Batch/Common/SampleDto.cs
+++ src/Application/Batch/Common/SampleDto.cs
@@ -1,12 +1,14 @@
 ﻿using brewjournal.Application.Common.Mappings;
 using brewjournal.Domain.Entities;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace brewjournal.Application.Batch.Common
 {
     public class SampleDto : IMapFrom<BatchSample>
     {
+        public int BatchId { get; set; }
+        public DateTime SampleDate { get; set; }
+        public long Gravity { get; set; }
+        public long Temperature { get; set; }
     }
 }
