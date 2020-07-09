7c1616b3f9a82bb58c5e8422ff25fffa6368277f
matt-goldman <mattgoldman1981@gmail.com>
2020-07-10 08:00:43 +1000

    Added recipes controller

---
+++ src/WebUI/Controllers/RecipesController.cs
@@ -0,0 +1,30 @@
+﻿using brewjournal.Application.Recipes.Commands.AddRecipe;
+using brewjournal.Application.Recipes.Common;
+using brewjournal.Application.Recipes.Queries.GetRecipe;
+using brewjournal.Application.Recipes.Queries.SearchRecipes;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace brewjournal.WebUI.Controllers
+{
+    public class RecipesController : ApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(RecipeDto recipe)
+        {
+            return await Mediator.Send(new AddRecipeCommand { viewModel = recipe });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RecipeDto>> Get(int id)
+        {
+            return await Mediator.Send(new GetRecipeQuery { Id = id });
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<RecipeSearchResultsVm>> Search(SearchRecipeQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
