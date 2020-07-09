123317d8326ab8b4a36eda22866547a1e6532aa1
Matt Goldman <MattGoldman@ssw.com.au>
2020-07-09 21:22:16 +1000

    added ingredients controller

---
+++ src/WebUI/Controllers/IngredientsController.cs
@@ -0,0 +1,30 @@
+﻿using brewjournal.Application.Ingredients.Commands.AddIngredient;
+using brewjournal.Application.Ingredients.Queries.Common;
+using brewjournal.Application.Ingredients.Queries.GetAllIngredients;
+using brewjournal.Application.Ingredients.Queries.SearchIngredients;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace brewjournal.WebUI.Controllers
+{
+    public class IngredientsController : ApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(AddIngredientCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IngredientsVm>> Get()
+        {
+            return await Mediator.Send(new GetAllIngredientsQuery());
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<IngredientsVm>> Search(string searchTerm)
+        {
+            return await Mediator.Send(new SearchIngredientsQuery { SearchTerm = searchTerm });
+        }
+    }
+}
